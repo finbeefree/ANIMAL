a25824c1f6f235c3c9a0c0384687d18afb1e733c
finbeefree <93152597+finbeefree@users.noreply.github.com>
2021-11-04 19:58:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        Animal rabbit = new Rabbit("Шаст", 5, 10, 10);
+        Animal rabbit = new Rabbit("Шаст", 2, 5, 5);
         rabbit.move();
         rabbit.sleep();
         rabbit.voice();
         rabbit.eat();
         System.out.println();
 
-        Animal gazelle = new Gazelle("Оливия", 10, 50, 90);
+        Animal gazelle = new Gazelle("Оливия", 10, 30, 60);
         gazelle.move();
         gazelle.sleep();
         gazelle.voice();
         gazelle.eat();
         System.out.println();
 
-        Animal fox = new Fox("Дороти", 20, 40, 50);
+        Animal fox = new Fox("Дороти", 15, 30, 70);
         fox.move();
         fox.sleep();
         fox.voice();
         fox.eat();
         System.out.println();
 
-        Animal wolf = new Wolf("Бакс", 30, 50, 60);
+        Animal wolf = new Wolf("Бакс", 20, 30, 70);
         wolf.move();
         wolf.sleep();
         wolf.voice();
         wolf.eat();
         System.out.println();
 
-        Animal bear = new Bear("Богдан", 40, 100, 70);
+        Animal bear = new Bear("Богдан", 20, 150, 70);
         bear.move();
         bear.sleep();
         bear.voice();
